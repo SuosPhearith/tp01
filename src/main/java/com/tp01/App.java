2fdf1543860d44e9a9668ae697f43ef003e11d00
SuosPhearith <suosphearith@gmail.com>
2024-03-22 17:13:51 +0700

    2

--- src/main/java/com/tp01/App.java
+++ src/main/java/com/tp01/App.java
@@ -4,10 +4,8 @@ package com.tp01;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
